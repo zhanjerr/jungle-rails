2f8e4f75afc2475f87b2b20253d6336c956ef6f5
jerry <jerry.h.zhan@gmail.com>
2017-05-14 13:03:21 -0400

    added admin page for managing categories

---
+++ app/controllers/admin/categories_controller.rb
@@ -0,0 +1,24 @@
+class Admin::CategoriesController < ApplicationController
+  def index
+    @categories = Category.order(id: :desc).all
+  end
+
+  def new
+    @category = Category.new
+  end
+
+  def create
+    @category = Category.new(category_param)
+    if @category.save
+      redirect_to [:admin, :categories], notice: 'Category created!'
+    else
+      render :new
+    end
+  end
+
+  private
+
+  def category_param
+    params.require(:category).permit(:name)
+  end
+end
